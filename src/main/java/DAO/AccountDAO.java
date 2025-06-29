72787b0739defca4fe37bd9d1d36c2f3a0ca1bf9
Marcel Castro <marcel88rmadrid@yahoo.com>
2025-06-29 02:10:17 +0000

    commit 4

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -10,8 +10,16 @@ import Util.ConnectionUtil;
 
 public class AccountDAO {
 
-// Requirements
-// 1: Our API should be able to process new User registrations. 
+/** Requirements
+  * 1: Our API should be able to process new User registrations. 
+  * As a user, I should be able to create a new Account on the endpoint POST localhost:8080/register. 
+  * The body will contain a representation of a JSON Account, but will not contain an account_id.
+  * The registration will be successful if and only if the username is not blank, the password is at least 4 characters long, 
+  * and an Account with that username does not already exist. If all these conditions are met, the response body should contain a JSON of the Account, 
+  * including its account_id. The response status should be 200 OK, which is the default. The new account should be persisted to the database.
+  * If the registration is not successful, the response status should be 400. (Client error)
+  */
+
 
     public Account insertNewAccount(Account acc){
 
@@ -64,17 +72,18 @@ public class AccountDAO {
 
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
-                acc2 = new Account(rs.getString("username"), rs.getString("password"));
+                acc2 = new Account(rs.getString("username"), 
+                                   rs.getString("password"));
             }
 
             if(account.username.equals(acc2.getUsername()) && account.password.equals(acc2.getPassword())){
-                return "User found, login succesful!";
+                return "User found. Login succesful!";
             }
             else{
                 return "User not found!";
             }
         }
-        
+
         catch(SQLException e){
             System.out.println(e.getMessage());
         }
